e92a1b46c0bfcbe05f0215a456f3d192524a40e1
ElizavetaRazumenko <razumenko99@mail.ru>
2023-01-11 21:24:19 +0300

    add: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+         n = -1 * n;
+    }
+    let num = n.toString();
+    let result = '';
+    let i = (num.length - 1);
+    while (i > -1) {
+        result += num[i];
+        i--;
+    } return +result;
 }
